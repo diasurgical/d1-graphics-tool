ed266caf82ef8744a3e45bd6a9a9075f3e20fa59
Anders Jenbo <anders@jenbo.dk>
2022-09-03 03:49:34 +0200

    Update release.ps1

--- releases/release.ps1
+++ releases/release.ps1
@@ -1,13 +1,13 @@
 #Requires -Version 7
 
 $ReleaseName = 'd1-graphics-tool'
-$ReleaseVersion = 'beta'
+$ReleaseVersion = '0.5.0'
 $ReleaseOs = 'windows'
 $ReleaseArchitecture = 'x64'
 $ReleaseZipFileName = "$ReleaseName-$ReleaseVersion-$ReleaseOs-$ReleaseArchitecture.zip"
 
-$QtPath = 'D:\a\d1-graphics-tool\Qt\6.2.4\msvc2019_64\bin'
-$ReleaseExePath = 'release\D1GraphicsTool.exe'
+$QtPath = 'C:\Qt\6.1.2\mingw81_64\bin'
+$ReleaseExePath = '..\build-D1GraphicsTool-Desktop_Qt_6_1_2_MinGW_64_bit-Release\release\D1GraphicsTool.exe'
 
 if( Test-Path -Path .\$ReleaseZipFileName ) {
     Remove-Item -Path .\$ReleaseZipFileName -Confirm:$false -Force
@@ -28,6 +28,11 @@ Copy-Item -Path $ReleaseExePath -Destination .\$ReleaseName
 
 # Copy Qt binaries
 windeployqt .\$ReleaseName
+# Copy other dependencies
+Copy-Item -Path $QtPath\libgcc_s_seh-1.dll -Destination .\$ReleaseName
+Copy-Item -Path $QtPath\libstdc++-6.dll -Destination .\$ReleaseName
+Copy-Item -Path $QtPath\libwinpthread-1.dll -Destination .\$ReleaseName
+
 
 Compress-Archive -Path .\$ReleaseName -DestinationPath $ReleaseZipFileName
 
