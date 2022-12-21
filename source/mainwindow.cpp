000b2a7592069f32cec515e42bb7092993713dd6
pionere <pionere@freemail.hu>
2022-12-21 10:00:41 +0100

    fix path extensions in open dialog - get rid of the unsupported option to open GIF/pcx files - add uppercase .CLX

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -142,7 +142,7 @@ void MainWindow::on_actionOpen_triggered()
 {
     QString openFilePath = QFileDialog::getOpenFileName(
         this, "Open Graphics", this->configuration->value("WorkingDirectory").toString(),
-        "CEL/CL2/CLX Files (*.cel *.CEL *.cl2 *.CL2 *.clx);;PCX Files (*.pcx *.PCX);;GIF Files (*.gif)");
+        "CEL/CL2/CLX Files (*.cel *.CEL *.cl2 *.CL2 *.clx *.CLX)");
 
     if (!openFilePath.isEmpty()) {
         this->openFile(openFilePath);
