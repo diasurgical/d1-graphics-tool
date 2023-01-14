63178f0d5886ba73646a22b19c2c298fb3cdf136
pionere <pionere@freemail.hu>
2023-01-14 11:13:28 +0100

    fix tileset cleanup

--- source/levelcelview.cpp
+++ source/levelcelview.cpp
@@ -1124,11 +1124,11 @@ void LevelCelView::cleanupSubtiles()
 
 void LevelCelView::cleanupTileset()
 {
-    QString framesReport;
-    this->removeUnusedFrames(framesReport);
-
     QString subtilesReport;
     this->removeUnusedSubtiles(subtilesReport);
+
+    QString framesReport;
+    this->removeUnusedFrames(framesReport);
 
     if (framesReport.isEmpty() && subtilesReport.isEmpty()) {
         framesReport = "Every subtile and frame are used.";
