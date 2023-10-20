e4127fd0c205cf50715ee32412697df5b01a2b6d
tetektoza <skociektoja@gmail.com>
2023-10-20 16:56:11 +0200

    Documentation: Add documentatioon to config.cpp
    Adds documentation to config.cpp, for functions that were missing it and
    also for newly added createDirectoriesOnPath().

--- source/config.cpp
+++ source/config.cpp
@@ -11,6 +11,13 @@
 static QJsonObject theConfig;
 QString Config::jsonFilePath;
 
+/**
+ * @brief Loads current configuration from the config .json file
+ *
+ *  This function loads current configuration from the config .json file.
+ *  It inserts specific values mapping them to keys, and creates path for the
+ *  config if user has deleted it, or runs app for the first time.
+ */
 void Config::loadConfiguration()
 {
     // create directories on the path if they do not exist
@@ -51,6 +58,9 @@ void Config::loadConfiguration()
     }
 }
 
+/**
+ * @brief Stores current configuration in the config .json file
+ */
 void Config::storeConfiguration()
 {
     QFile saveJson(jsonFilePath);
@@ -60,6 +70,17 @@ void Config::storeConfiguration()
     saveJson.close();
 }
 
+/**
+ * @brief Creates directories on certain path
+ *
+ * This function creates directories on certain path. Path location depends on
+ * if user is working on Windows or Mac/Linux.
+ *
+ * If the user works on Windows, it will save it under AppData/.config/[...].
+ * On any other OS it will save it under /home/user/.config/diasurgical/[...]
+ *
+ * @return Returns true if path has been created or already existed - false otherwise
+ */
 bool Config::createDirectoriesOnPath()
 {
     jsonFilePath = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
@@ -67,11 +88,26 @@ bool Config::createDirectoriesOnPath()
     return QDir().mkpath(jsonFilePath);
 }
 
+/**
+ * @brief Retrieves value from .json config file
+ *
+ * This function retrieves value from .json config file by the value
+ * specified by name parameter.
+ *
+ * @return Returns QJsonValue containing value of the parameter specified
+ * by "name" key, otherwise if not found - returns QJsonValue::Undefined
+ */
 QJsonValue Config::value(const QString &name)
 {
     return theConfig.value(name);
 }
 
+/**
+ * @brief Inserts value in .json config file
+ *
+ * This function inserts value into .json config file, mapping it with
+ * the key specified in parameters.
+ */
 void Config::insert(const QString &key, const QJsonValue &value)
 {
     theConfig.insert(key, value);
