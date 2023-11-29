aa4264bb4c2f24d641be6d33d811d59852fb8aa3
tetektoza <tetektoza@gmail.com>
2023-11-29 23:14:09 +0100

    Command: Add documentation to existing methods
    Added documentation to existing methods in Command class.

--- source/command.cpp
+++ source/command.cpp
@@ -1,10 +1,21 @@
 #include "command.h"
 
+/**
+ * @brief Sets if a command is obsolete or not
+ *
+ * This function sets if a command is obsolete or not.
+ * This flag will be used in UndoStack to check if a
+ * command should be popped whenever there will be undo/redo
+ * operation used, or user pushes any command onto the stack.
+ */
 void Command::setObsolete(bool isObsolete)
 {
     m_isObsolete = isObsolete;
 }
 
+/**
+ * @brief Returns if a command is obsolete or not
+ */
 bool Command::isObsolete() const
 {
     return m_isObsolete;
