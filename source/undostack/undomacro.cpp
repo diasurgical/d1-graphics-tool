1f8ac3274d3d10aae3afca3af5e10d2d367f91e1
tetektoza <tetektoza@gmail.com>
2024-01-24 23:33:16 +0100

    Undostack: Introduce UndoMacro

---
+++ source/undostack/undomacro.cpp
@@ -0,0 +1,48 @@
+#include "undomacro.h"
+
+#include <utility>
+
+UserData::UserData(QString labelText, QString cancelButtonText, std::pair<int, int> &&minMax)
+    : m_labelText(std::move(labelText))
+    , m_cancelButtonText(std::move(cancelButtonText))
+    , m_minMax(minMax)
+{
+}
+
+UndoMacroFactory::UndoMacroFactory(UserData &&userData)
+    : m_userData(std::make_unique<UserData>(userData.labelText(), userData.cancelButtonText(), std::make_pair(userData.min(), userData.max())))
+{
+}
+
+void UndoMacroFactory::add(std::unique_ptr<Command> cmd)
+{
+    m_commands.push_back(std::move(cmd));
+}
+
+UndoMacro::UndoMacro(std::unique_ptr<UserData> userData, std::pair<int, int> rangeIdxs)
+    : m_userData(std::move(userData))
+    , m_rangeIdxs(rangeIdxs)
+{
+}
+
+UndoMacro::UndoMacro(UndoMacro &&undoMacro) noexcept
+    : m_userData(std::move(undoMacro.m_userData))
+    , m_rangeIdxs(undoMacro.m_rangeIdxs)
+{
+}
+
+UndoMacro &UndoMacro::operator=(UndoMacro &&undoMacro) noexcept
+{
+    m_userData = std::move(undoMacro.m_userData);
+    m_rangeIdxs = undoMacro.m_rangeIdxs;
+    return *this;
+}
+
+void UndoMacro::setLastIndex(int index)
+{
+    m_rangeIdxs.second = index;
+    /* TODO: later on, if/when we will implement more types of macros, we could use some update method of UserData
+     * instead of using setters directly
+     */
+    m_userData->setMax((m_rangeIdxs.second - m_rangeIdxs.first) + 1);
+}
---
+++ source/undostack/undomacro.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "command.h"
+#include <QString>
+#include <memory>
+#include <string_view>
+#include <vector>
+
+class UserData {
+private:
+    QString m_labelText;
+    QString m_cancelButtonText;
+    std::pair<int, int> m_minMax;
+
+public:
+    UserData(QString labelText, QString cancelButtonText, std::pair<int, int> &&minMax);
+    ~UserData() = default;
+
+    [[nodiscard]] int min() const
+    {
+        return m_minMax.first;
+    }
+    [[nodiscard]] int max() const
+    {
+        return m_minMax.second;
+    }
+    [[nodiscard]] const QString &labelText() const
+    {
+        return m_labelText;
+    }
+    [[nodiscard]] const QString &cancelButtonText() const
+    {
+        return m_cancelButtonText;
+    }
+    void setMin(int min)
+    {
+        m_minMax.first = min;
+    }
+    void setMax(int max)
+    {
+        m_minMax.second = max;
+    }
+};
+
+class UndoMacroFactory {
+private:
+    std::unique_ptr<UserData> m_userData;
+    std::vector<std::unique_ptr<Command>> m_commands;
+
+public:
+    UndoMacroFactory(UserData &&userData);
+    ~UndoMacroFactory() = default;
+
+    void add(std::unique_ptr<Command> cmd);
+    [[nodiscard]] std::vector<std::unique_ptr<Command>> &cmds()
+    {
+        return m_commands;
+    };
+    [[nodiscard]] std::unique_ptr<UserData> &userdata()
+    {
+        return m_userData;
+    };
+};
+
+class UndoMacro {
+private:
+    std::unique_ptr<UserData> m_userData;
+    std::pair<int, int> m_rangeIdxs;
+
+public:
+    UndoMacro(std::unique_ptr<UserData> userData, std::pair<int, int> rangeIdxs);
+    UndoMacro(UndoMacro &&undoMacro) noexcept;
+    UndoMacro &operator=(UndoMacro &&undoMacro) noexcept;
+    ~UndoMacro() = default;
+
+    [[nodiscard]] std::unique_ptr<UserData> &userdata()
+    {
+        return m_userData;
+    };
+    [[nodiscard]] int beginIndex() const
+    {
+        return m_rangeIdxs.first;
+    }
+    [[nodiscard]] int lastIndex() const
+    {
+        return m_rangeIdxs.second;
+    }
+    void setLastIndex(int index);
+};
